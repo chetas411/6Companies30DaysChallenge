ab503573b3c2190de06e29e4b1bea13599a82f99
Chetas Sharma <sharmachetas.111@gmail.com>
2022-01-23 17:57:00 +0530

    Day 23, Q4 done

---
+++ Microsoft/Stocks_Span.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+    public:
+    //Function to calculate the span of stockâ€™s price for all n days.
+    vector <int> calculateSpan(int price[], int n){
+       // Your code here
+       stack<int> s;
+       vector<int> ans;
+       for(int i=0;i<n;i++){
+           if(i == 0){
+               s.push(i);
+               ans.push_back(1);
+               continue;
+           }
+           while(!s.empty() && price[s.top()] <= price[i]){
+               s.pop();
+           }
+           if(s.empty()){
+               ans.push_back(i+1);
+           }
+           else{
+               ans.push_back(i-s.top());
+           }
+           s.push(i);
+       }
+       return ans;
+    }
+};
